7a908fb1a7a9d4022d58e720cca97455ec4b9fb3
Rodrigo Nascimento <rodrigo.nascimento@castgroup.com.br>
2020-04-24 14:40:59 -0300

    first commit finalize project

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { FormsModule }   from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+
+import { TodoListComponent } from './todo-list.component';
+import { TodoService } from './todo.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [TodoService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
